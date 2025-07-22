180ee6108aa49d605232772c3b94b6f532a852e5
Connor Garey <connorgarey1999@gmail.com>
2025-07-22 16:15:57 +0100

    File will now open.

--- src/command-parser.zig
+++ src/command-parser.zig
@@ -1,32 +1,40 @@
 const std = @import("std");
 const log = std.log;
+const linux = std.os.linux;
 
 const available_commands_enum = @import("read-from-client.zig").AvailableInputs;
 
 pub const CommandParser = struct {
-    pub fn parseCommand(chosen_command: available_commands_enum) void {
+    pub fn parseCommand(chosen_command: available_commands_enum, client_fd: *const usize) void {
         switch (chosen_command) {
             .unknown => {
-                unknownCommand();
+                unknownCommand(client_fd);
             },
             .cpu => {
-                cpuCommand();
+                cpuCommand(client_fd);
             },
             .help => {
-                helpCommand();
+                helpCommand(client_fd);
             },
         }
     }
-    fn unknownCommand() void {
+    fn unknownCommand(_: *const usize) void {
         // TODO
         log.debug("(unknownCommand) Called!", .{});
     }
-    fn helpCommand() void {
+    fn helpCommand(_: *const usize) void {
         // TODO
         log.debug("(helpCommand) Called!", .{});
     }
-    fn cpuCommand() void {
+    fn cpuCommand(_: *const usize) void {
         // TODO
         log.debug("(cpuCommand) Called!", .{});
+        
+        // var buffer = std.heap.page_allocator.alloc(u8, 4096);
+        // defer std.heap.page_allocator.free(buffer);
+
+        const file_id: usize = linux.open("/proc/stat", .{}, 0);
+        defer _ = linux.close(@intCast(file_id));
+        log.debug("(cpuCommand)File: {d}", .{file_id});
     }
 };
