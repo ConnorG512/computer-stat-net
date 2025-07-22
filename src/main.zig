5d705c16d33ea82162899e9f729d4bfcf882f79c
Connor Garey <connorgarey1999@gmail.com>
2025-07-22 11:50:52 +0100

    Changed command_parser_t to CommandParser

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,7 @@ const log = std.log;
 const socket_t = @import("socket.zig").Socket;
 const to_client_t = @import("write-to-client.zig").ToClient;
 const read_from_clinet_t = @import("read-from-client.zig").Reader;
-const command_parser_t = @import("command-parser.zig").CommandParser;
+const CommandParser = @import("command-parser.zig").CommandParser;
 
 pub fn main() !void {
     var current_socket = socket_t.init();
@@ -19,7 +19,7 @@ pub fn main() !void {
         var buffer: [32]u8 = undefined;
         const input_len = read_from_clinet_t.readMessageFromClient(&client_fd, buffer[0..]);
         const client_chosen_command = read_from_clinet_t.identifyCommand(buffer[0..input_len - 1]);
-        command_parser_t.parseCommand(client_chosen_command);
+        CommandParser.parseCommand(client_chosen_command);
     }
 }
 
