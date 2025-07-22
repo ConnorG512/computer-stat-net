2c9d4dd5979944433ce4a07189f80a2f837e7385
Connor Garey <connorgarey1999@gmail.com>
2025-07-22 09:59:46 +0100

    Tweaks to the user inputs enum.

--- src/read-from-client.zig
+++ src/read-from-client.zig
@@ -2,8 +2,8 @@ const std = @import("std");
 const linux = std.os.linux;
 const log = std.log;
 
-pub const AvailableCommands = enum {
-    none,
+pub const AvailableInputs = enum {
+    unknown,
     help,
 };
 
@@ -12,14 +12,14 @@ pub const Reader = struct {
        const bytes_read: usize = linux.read(@intCast(client_fd.*), buffer.ptr, buffer.len); 
        return bytes_read;
     }
-    pub fn identifyCommand(buffer: []const u8) AvailableCommands {
+    pub fn identifyCommand(buffer: []const u8) AvailableInputs {
         if (std.mem.eql(u8, buffer, "help")) {
             log.debug("(identifyCommand) help command hit.", .{});
             return .help;
         }
         else {
             log.debug("(identifyCommand) none command hit.", .{});
-            return .none;
+            return .unknown;
         }
 
     }
