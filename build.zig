ebcd7dcde4219ef45a1074e81858e511db70c045
Connor Garey <connorgarey1999@gmail.com>
2025-07-24 20:19:49 +0100

    Commended out unneeded libc link

--- build.zig
+++ build.zig
@@ -40,7 +40,7 @@ pub fn build(b: *std.Build) void {
     // step when running `zig build`).
     b.installArtifact(exe);
     
-    exe.linkLibC();
+    // exe.linkLibC();
 
     // This *creates* a Run step in the build graph, to be executed when another
     // step is evaluated that depends on it. The next line below will establish
