8c507f1763f9db844484dcb49c4270e8ac2cde15
Connor Garey <connorgarey1999@gmail.com>
2025-07-24 19:59:44 +0100

    Removed old commented main function.

--- src/main.zig
+++ src/main.zig
@@ -29,17 +29,3 @@ pub fn main() !void {
         }
     }
 }
-
-// var current_socket = socket_t.init();
-// defer current_socket.closeSocket();
-// current_socket.startSocket();
-// while (true) {
-//     const client_fd = current_socket.acceptConnection();
-//     defer _ = std.os.linux.close(@intCast(client_fd));
-//     log.debug("client_fd: {d}.", .{client_fd});
-//     try to_client_t.sendLoginMessage(&client_fd);
-//     var buffer: [32]u8 = undefined;
-//     const input_len = read_from_clinet_t.readMessageFromClient(&client_fd, buffer[0..]);
-//     const client_chosen_command = read_from_clinet_t.identifyCommand(buffer[0..input_len - 1]);
-//     try CommandParser.parseCommand(client_chosen_command, &client_fd);
-// }
